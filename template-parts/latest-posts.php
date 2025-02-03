18957f63f69a046c3feab64ebbf3737c77328937
gianluca <gianlucaquaranta92@gmail.com>
2025-02-03 12:29:15 +0100

    -mobile style

--- template-parts/latest-posts.php
+++ template-parts/latest-posts.php
@@ -3,40 +3,40 @@
 
 <section class="latest-posts">
     <div class="page-width">
-    <h2>1. Our recent reviews of the best online casinos</h2>
-    <div>
+        <h2>1. Our recent reviews of the best online casinos</h2>
+        <div>
 
-            <?php
-        // Custom query to get the latest posts
-        $args = array(
-            'post_type'      => 'post',      
-            'posts_per_page' => 5,           
-            'orderby'        => 'date',      
-            'order'          => 'DESC',   
-        );
-
-        $latest_posts = new WP_Query( $args );
-
-
-        if ( $latest_posts->have_posts() ) :
-            echo '<div class="posts-container">'; 
-
-            // Loop through the posts
-            while ( $latest_posts->have_posts() ) : $latest_posts->the_post();
-                ?>
-                
-                    <a class="single-post" href="#"><?php the_title(); ?></a>
-                
                 <?php
-            endwhile;
+            // Custom query to get the latest posts
+            $args = array(
+                'post_type'      => 'post',      
+                'posts_per_page' => 5,           
+                'orderby'        => 'date',      
+                'order'          => 'DESC',   
+            );
 
-            echo '</div>'; 
-            wp_reset_postdata();
-        else :
-            echo '<p>No posts found.</p>'; 
-        endif;
-        ?>
+            $latest_posts = new WP_Query( $args );
 
-    </div>
+
+            if ( $latest_posts->have_posts() ) :
+                echo '<div class="posts-container">'; 
+
+                // Loop through the posts
+                while ( $latest_posts->have_posts() ) : $latest_posts->the_post();
+                    ?>
+                    
+                        <a class="single-post" href="#"><?php the_title(); ?></a>
+                    
+                    <?php
+                endwhile;
+
+                echo '</div>'; 
+                wp_reset_postdata();
+            else :
+                echo '<p>No posts found.</p>'; 
+            endif;
+            ?>
+
+        </div>
     </div>
 </section>
